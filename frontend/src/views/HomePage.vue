9627a262f74a80096dd348df44c59a05f790294f
bradleySchaeferComputerEng <97250667+bradleySchaeferComputerEng@users.noreply.github.com>
2023-04-23 15:09:49 -0400

    Fixed basic components

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -3,13 +3,10 @@
 </script>
 
 <template>
-  <div>
-    <header class="front-page">
-      <div style="text-align: center; padding-top: 170px;">
-        <img src="../../public/Logos/logo.png" alt="Logo" style="width: 18%;">
-        <h2 style="padding-top: 140px;">Wanna Join the Club?</h2>
-      </div>
-    </header>
+  <div id="homepage">
+    <div class="front-page">
+      <img class="front-logo" src="../../public/Logos/logo.png" alt="Logo">
+    </div>
     <content>
       <div>
         <h3>We are Mary Lake Ski Club, a group of University age best friends from Muskoka who spend our summer nights rushing home from work to catch an evening sunset ski. The slalom ski team consists of Brad, Teagen, David, Janine & Emily, but we love throwing photos up of our best friends who prefer other water sports too ;) We started skiing together summer of 2020 when hanging out indoors was a challenge, and we quickly became best friends & avid waterskiers. This insta is a fun way for us to share our summer adventures & favourite boat memories. Photos are taken mostly by @davo71, but also @emily.mcdougs, & a few from everyone. Thanks for following along as we countdown the days till summer!! 🌅🪩🕺🏼</h3>
@@ -22,17 +19,18 @@
 </template>
 
 <style scoped>
-html, body {
-    margin: 0;
-    padding: 0;
-    font-family: Arial, Helvetica, sans-serif;
+.front-page {
+  background-image: url('../../public/Cool_Shots/drone_turning_shot.jpg');
+  background-position: center center;
+  background-size: cover;
+  height: 700px;
+  text-align: center;
 }
 
-.front-page {
-    background-image: url('../../public/Cool_Shots/drone_turning_shot.jpg');
-    background-position: center center;
-    background-size: cover;
-    height: 100vh;
+.front-logo {
+  min-width: 200px;
+  max-width: 20vw;
+  padding-top: 170px;
 }
 
 h2 {
