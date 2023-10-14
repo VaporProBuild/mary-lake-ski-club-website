921ad74b91ec0321f8fa0abb2b61fc950be1177c
bradleySchaeferComputerEng <97250667+bradleySchaeferComputerEng@users.noreply.github.com>
2023-10-14 17:12:08 -0400

    Removed unused import

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import VitePluginGhPages from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    VitePluginGhPages({})
+    vue()
   ],
   resolve: {
     alias: {
