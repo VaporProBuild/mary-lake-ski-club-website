1a5702786a0770917a0b31f3a9e8a82a1eab2919
bradleySchaeferComputerEng <97250667+bradleySchaeferComputerEng@users.noreply.github.com>
2023-12-05 22:05:29 -0500

    Footer Updated

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="footer">
+    <p>Connect With Us!!!</p>
+    <p><a href="mailto:marylakeskiclub@gmail.com">marylakeskiclub@gmail.com</a></p>
+    <p>Follow our socials <a href="https://www.instagram.com/marylakeskiclub/" target="_blank">@marylakeskiclub</a></p>
+  </div>
+</template>
+
+<style scoped>
+.footer {
+  background-image: url('/Cool_Shots/drone_boat_blue.jpg');
+  background-position: center center;
+  background-size: cover;
+  padding: 70px 70px;
+  color: white;
+}
+
+.footer a {
+  text-decoration: none;
+  color: white;
+}
+
+</style>
